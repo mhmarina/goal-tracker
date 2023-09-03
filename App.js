e4bbc472e4d97a0e7123db0e667b095cf249de91
mhmarina <marinamagedsamuel@gmail.com>
2023-09-03 01:23:45 -0700

    added functionality and used FlatList to optimize scrolling

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import {  StyleSheet, 
+          Text, 
+          View, 
+          Button, 
+          TextInput, 
+          ScrollView, 
+          FlatList} from 'react-native';
+
+import {useState} from 'react';
 
 export default function App() {
+
+  const [enteredText, setText] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  function goalInputHandler(text){
+    setText(text);
+  }
+
+  function addGoalHandler(){
+    setGoals((currentGoals) => 
+      [...currentGoals, enteredText])
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="what's your next goal...?"/>
-        <Button title="Add Goal"/>
+        <TextInput 
+                  style={styles.textInput} placeholder="what's your next goal...?"
+                  placeholderTextColor= '#7a676a' 
+                  onChangeText={goalInputHandler}/>
+        <Button 
+          title="Add Goal" onPress={addGoalHandler}/>
       </View>
-      <View style={styles.goalsContainer}>
-        <Text>list of goals</Text>
+      <View  style={styles.goalsContainer}>
+          <FlatList data={goals} renderItem={itemData => 
+            {return(<Text style={styles.goalItem}>{itemData.item}</Text>)
+            }}/>
       </View>
     </View>
   );
@@ -19,6 +46,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
+    backgroundColor: '#211b1c',
   },
   inputContainer: {
     flexDirection: 'row',
@@ -27,16 +55,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
+    borderBottomColor: 'white',
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
+    borderColor: 'white',
+    color: '#ff6188',
     width: '70%%',
     marginRight: 8,
     padding: 8,
   },
   goalsContainer: {
     flex: 10,
+  },
+  goalItem: {
+    marginBottom: 10,
+    backgroundColor: '#ff6188',
+    padding: 8,
+  },
+  addButton: {
+    backgroundColor: '#ff6188',
   }
 });
